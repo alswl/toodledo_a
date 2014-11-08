fd0eb73da570d980a2d2e317621e69de153a1830
alswl <alswlx@gmail.com>
2014-11-08 19:58:45 +0800

    works

---
+++ toodledo_a.py
@@ -0,0 +1,111 @@
+# coding=utf-8
+
+import csv
+import argparse
+import itertools
+from collections import OrderedDict
+import sys
+from datetime import datetime, timedelta, time
+from dateutil import relativedelta
+from dateutil.parser import parse
+
+from unicode_csv import UnicodeReader, UnicodeWriter
+
+
+class Todo(object):
+
+    @staticmethod
+    def from_fields(fields):
+        that = Todo()
+        that.task = fields[0]
+        that.folder = fields[1]
+        that.context = fields[2]
+        that.goal = fields[3]
+        that.location = fields[4]
+        that.start_date = parse(fields[5])
+        that.start_time = parse(fields[6])
+        that.due_date = parse(fields[7])
+        that.duet_ime = parse(fields[8])
+        that.repeat = fields[9]
+        that.length = int(fields[10]) if fields[10] else None
+        that.timer = int(fields[11]) if fields[11] else None
+        that.priority = fields[12]
+        that.tag = fields[13]
+        that.status = fields[14]
+        that.star = fields[15]
+        that.note = fields[16]
+        that.completed = parse(fields[17])
+        return that
+
+
+def get_todos_from_reader(reader):
+    todos = []
+    for i, fields in enumerate(reader):
+        if i == 0:
+            continue
+        todo = Todo.from_fields(fields)
+        todos.append(todo)
+    return sorted(todos,key=lambda x: x.completed)
+
+
+def get_latest_todos(todos, days=7):
+    today = datetime.today()
+    return [x for x in todos if today - x.completed <= timedelta(days=days)]
+
+
+def group_by_value(todos, value_name, value_list):
+    if len(todos) == 0:
+        return []
+    last_todo = todos[0]
+    value_dict = OrderedDict()
+    for value in value_list:
+        value_dict[value] = 0
+    csv_res = [[None, value_dict]]
+    for todo in todos:
+        if todo.completed - last_todo.completed == timedelta(0):
+            row = csv_res[-1]
+            row[0] = datetime.strftime(todo.completed, '%Y-%m-%d')
+        else:
+            value_dict = OrderedDict()
+            for value in value_list:
+                value_dict[value] = 0
+            row = [datetime.strftime(todo.completed, '%Y-%m-%d'), value_dict]
+            csv_res.append(row)
+        row[1][getattr(todo, value_name)] += todo.timer or 0
+        last_todo = todo
+    return csv_res
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--days', type=int, default=7)
+    arg_parser.add_argument('path')
+    args = arg_parser.parse_args()
+    todo_csv_path = sys.argv[1]
+
+    todo_csv_str = open(args.path, 'rb')
+    reader = UnicodeReader(todo_csv_str)
+    todos = get_todos_from_reader(reader)
+    latest_todo = get_latest_todos(todos, days=args.days)
+    folders = list(set([x.folder for x in todos]))
+    contexts = list(set([x.context for x in todos]))
+
+    group_by_folder_csv_path = args.path + '_group_by_folder.csv'
+    group_by_folder_csv = open(group_by_folder_csv_path, 'wb')
+    res = group_by_value(latest_todo, 'folder', folders)
+    print 'write: %s' % group_by_folder_csv_path
+    group_by_folder_csv_writer = UnicodeWriter(group_by_folder_csv)
+
+    row = ['complete']
+    row.extend(folders)
+    group_by_folder_csv_writer.writerow(row)
+
+    for row in res[1:]:
+        output_row = [row[0]]
+        for _, v in row[1].items():
+            output_row.append(str(v))
+        group_by_folder_csv_writer.writerow(output_row)
+
+
+if __name__ == '__main__':
+    main()
